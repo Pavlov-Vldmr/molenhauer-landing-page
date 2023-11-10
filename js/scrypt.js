c843dd1cb049da1e71330dfe2b73d6d69f3583c0
Pavlov-Vldmr <vpavlov95007@gmail.com>
2023-11-10 14:14:16 +0300

    swiper slider and adaptive

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,44 +1,135 @@
 // $(document).ready(function () {
-// const benefitSlides = $(".benefit-slider-item");
-// const benefitNext = $(".benefit-next");
-// const benefitPrev = $(".benefit-prev");
-// console.log(benefitNext)
-const benefitSliderDotsText = $(".benefit_slider-dot");
-benefitSliderDotsText.each((el) => {
-  // console.log(el)
-  console.log(benefitSliderDotsText[el].children[0].textContent);
-  console.log(benefitSliderDotsText[el].children[1].textContent);
-});
 
-const swiper = new Swiper(".benefitSwiper", {
-  // Optional parameters
+const benefitSwiper = new Swiper(".benefitSwiper", {
   direction: "horizontal",
   loop: true,
-  bulletClass: ".benefit_slider-dot",
-  bulletActiveClass: ".benefit_slider-dot-active",
-  // If we need pagination
+  effect: "fade",
+  slidesPerView: 1,
+  fadeEffect: {
+    crossFade: true, 
+  }, 
+  autoplay: {
+    delay: 8000,
+  },
   pagination: {
     el: ".benefit-swiper-pagination",
     clickable: true,
+    type: "bullets",
+    bulletClass: "benefit_slider-dot",
+    bulletActiveClass: "benefit_slider-dot-active",
+    bulletElement: "button",
     renderBullet: function (index, className) {
+      const benefitSliderDotsText = $(".benefit_slider-dot");
       return (
-        '<button class="benefit_slider-dot ' +  className +  '">' +
-            "<span>" +  (benefitSliderDotsText[index].children[0].textContent) +  "</span> <span>"+ (benefitSliderDotsText[index].children[1].textContent) + '</span>' +
+        '<button class="benefit_slider-dot ' +
+        className +
+        '">' +
+        "<span>" +
+        benefitSliderDotsText[index].children[0].textContent +
+        "</span> <p>" +
+        benefitSliderDotsText[index].children[1].textContent +
+        "</p>" +
         "</button>"
       );
     },
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".benefit-next",
     prevEl: ".benefit-prev",
   },
 });
 
-// benefitNext.on("click", function () {
-//   console.log("test");
-// });
+const expirienceSwiper = new Swiper(".expirienceSwiper", {
+  direction: "horizontal",
+  // loop: true,
+  slidesPerView: 6,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      grabCursor: true,
+    },
+    640: {
+      slidesPerView: 2,
+      grabCursor: true,
 
-// swiper();
-// });
+      // spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      grabCursor: true,
+
+      // spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+    },
+  },
+});
+
+const keyPlayerSwiper = new Swiper(".keyPlayerSwiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  grabCursor: true,
+
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      grid: {
+        rows: 3,
+      },
+    },
+    425: {
+      slidesPerView: 2,
+      grabCursor: true,
+
+      // spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      grabCursor: true,
+
+      // spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+
+  navigation: {
+    nextEl: ".key-player__nav-next-button",
+    prevEl: ".key-player__nav-prev-button",
+  },
+});
+
+const clientsSaySwiper = new Swiper(".clientsSaySwiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  grabCursor: true,
+  effect: "fade",
+
+  fadeEffect: {
+    crossFade: true, 
+  }, 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".client-say__nav-next-button",
+    prevEl: ".client-say__nav-prev-button",
+  },
+});
+
+
